0ab1061b4873467669b6cf868e640d277670b1dc
Michael Oria <Michael_Oria@me.com>
2018-06-09 14:05:19 -0400

    Heroku Commit.

--- app-prod.js
+++ app-prod.js
@@ -200,7 +200,7 @@ function processLoginCallback(req, username, password, done) {
 
 }	 
 
-  // app.use(require('morgan')('combined'));
+  app.use(require('morgan')('combined'));
 	app.set('view engine', 'ejs')
 	app.use(bodyParser.json())
 	app.use(bodyParser.urlencoded({ extended: true }));
@@ -249,7 +249,7 @@ app.set('view engine', 'ejs');
 
 
 // Logging, Parsing, and Session Handling.
-// app.use(require('morgan')('combined'));
+app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
