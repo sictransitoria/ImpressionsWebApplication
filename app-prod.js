ea1a6f42ecdddaa50e1a44570edd6443ad01ef06
Michael Oria <Michael_Oria@me.com>
2018-06-09 14:37:35 -0400

    Heroku Commit.

--- app-prod.js
+++ app-prod.js
@@ -32,7 +32,7 @@ const Strategy = require('passport-local').Strategy;
 const passport = require('passport');
 
 // Server Port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Connect to Database
 const Op = Sequelize.Op
